f9bfb4ee937857da9005ff6c9ee953b9047ffc78
Marzia Fatarella <marziafatarella@gmail.com>
2020-06-16 13:43:34 +0200

    creazione file

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>Google FAQ</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <div class="container">
+            <?php
+
+            ?>
+        </div>
+    </body>
+</html>
